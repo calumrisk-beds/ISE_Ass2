98e69a7636136932ba3db8f18faeeaa20351198c
calumrisk-beds <calum.risk@study.beds.ac.uk>
2020-05-12 14:10:32 +0100

    SQLite User Samples
    Sample of using SQLite and __init__

---
+++ SharedPower/sqlite_testing.py
@@ -0,0 +1,131 @@
+import sqlite3
+from SharedPower.test_users  import User
+
+db = 'shared_power.db'
+
+conn = sqlite3.connect(db)
+c = conn.cursor()
+
+def create_users_table():
+    c.execute("""CREATE TABLE users (
+                usr_id text PRIMARY KEY,
+                pwrd text,
+                usr_type text,
+                first_name text,
+                last_name text,
+                add1 text,
+                add2 text,
+                add3 text,
+                add4 text,
+                post_code text,
+                tel_no text,
+                wallet integer 
+            )""")
+    conn.commit()
+    conn.close()
+
+
+def create_sample_user_1():
+    c.execute("""INSERT INTO users VALUES (
+                'joesbloggs',
+                'secure',
+                'tool_owner',
+                'Joe',
+                'Bloggs',
+                '1 Place',
+                'Road',
+                'Luton',
+                'Beds',
+                'LU3 3AA',
+                '0777777777',
+                0          
+            )""")
+    conn.commit()
+    conn.close()
+
+def create_sample_user_2():
+    usr = User(
+        'johnsmith',
+        'pass',
+        'tool_user',
+        'John',
+        'Smith',
+        'Building',
+        'Street',
+        'Milton Keynes',
+        'Bucks',
+        'MK1 1SW',
+        '01234567890',
+        100)
+
+    c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+              (usr.usr_id, usr.pwrd, usr.usr_type, usr.first_name,
+               usr.last_name, usr.add1, usr.add2, usr.add3,
+               usr.add4, usr.post_code, usr.tel_no, usr.wallet))
+
+    conn.commit()
+    conn.close()
+
+def create_sample_user_3():
+    usr = User(
+        'marywilson',
+        'strong',
+        'dispatch_rider',
+        'Mary',
+        'Wilson',
+        'Craggy House',
+        'Clare Street',
+        '',
+        'Belfast',
+        'BE1 2ZX',
+        '0800800800',
+        50
+    )
+
+    c.execute("""INSERT INTO users VALUES (
+               :usr_id, :pwrd, :usr_type, :first_name,
+               :last_name, :add1, :add2, :add3,
+               :add4, :post_code, :tel_no, :wallet)""",
+              {'usr_id': usr.usr_id,
+               'pwrd': usr.pwrd,
+               'usr_type': usr.usr_type,
+               'first_name': usr.first_name,
+               'last_name': usr.last_name,
+               'add1': usr.add1,
+               'add2': usr.add2,
+               'add3': usr.add3,
+               'add4': usr.add4,
+               'post_code': usr.post_code,
+               'tel_no': usr.tel_no,
+               'wallet': usr.wallet})
+
+    conn.commit()
+    conn.close()
+
+def sample_select_1():
+    c.execute("SELECT * FROM users WHERE usr_id='joesbloggs'")
+    print(c.fetchone())
+    conn.commit()
+    conn.close()
+
+def sample_select_2():
+    c.execute("SELECT * FROM users WHERE usr_id=?", ('johnsmith',))
+    print(c.fetchone())
+    conn.commit()
+    conn.close()
+
+def sample_select_3():
+    c.execute("SELECT * FROM users WHERE usr_id=:usr_id", {'usr_id': 'marywilson'})
+    print(c.fetchone())
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    #create_users_table()
+    #create_sample_user()
+    #sample_select()
+    #create_sample_user_2()
+    #create_sample_user_3()
+    #sample_select_2()
+    sample_select_3()
+
---
+++ SharedPower/user_factory.py
@@ -0,0 +1,9 @@
+class _Tool_User(object):
+    pass
+
+class _Tool_User(object):
+    pass
+
+def user_factory(user_type):
+    if user_type == 'tool_owner':
+        return _Tool_User()
